c166d17bbd186df9fb55d56e23a0f63e0899a83c
Hariharan Gopinath <tharanihari26@gmail.com>
2025-07-08 17:29:01 +0200

    add git action files

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/logger/log_config.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/logger/log_config.py
@@ -1,12 +1,16 @@
 # log_config.py
 import logging.config
-import yaml
 import os
 
-def setup_logging(config_path='logging_config.yaml', default_level=logging.INFO):
+import yaml
+
+parent_path = os.path.dirname(os.path.abspath(__file__))
+default_config_file_path = os.path.join(parent_path, 'logging_config.yaml')
+
+def setup_logging(config_path=default_config_file_path, default_level=logging.INFO):
     """
     Sets up logging using a YAML config file.
-    
+
     Parameters:
         config_path (str): Path to the YAML logging config file.
         default_level (int): Fallback logging level if config file not found.
