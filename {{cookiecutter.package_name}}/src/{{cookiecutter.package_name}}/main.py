6cf8ab01d3b143c87998a949efbb5d6419f05c09
Hariharan Gopinath <tharanihari26@gmail.com>
2025-07-09 09:38:23 +0200

    add run command

--- {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/main.py
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/main.py
@@ -1,10 +1,16 @@
 # some_module.py
-from .logger.log_config import setup_logging
 import logging
+
+from .logger.log_config import setup_logging
 
 setup_logging()  # or setup_logging("custom/path/to/logging.yaml")
 
-logger = logging.getLogger(__name__)
 
-logger.debug("Debug message")
-logger.info("Info message")
+def main():
+    logging.info("Starting the application...")
+    # Your application logic here
+    logging.info("Application finished successfully.")
+
+if __name__ == "__main__":
+    main()
+    logging.info("Exiting the application.")
